943a63f2a147da4369cc5056ddcbc302b620f910
abdulla0207 <abdulla.ermatov0407@gmail.com>
2022-04-02 00:14:23 +0500

    Saves the tasks to data base

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const path = require('path')
-
+const validate = require('validate.js')
 const PRIORITIES = require('./helper').priorities
 
 // Port number
@@ -12,14 +12,43 @@ let a = "HELLO"
 //Load View Engine
 app.set('view engine', 'pug')
 
+app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static('public'))
 
-app.route('/').get((req, res) => {
-    res.render('index', { priorities: PRIORITIES })
+// Loads the homepage
+app.get('/', (req, res) => {
+    if (req.query.received == 'yes') {
+        res.render('index', { priorities: PRIORITIES, answer: req.query.received })
+    } else {
+        res.render('index', { priorities: PRIORITIES })
+    }
 })
+// When add button clicked, it saves the information of inputs
+// to tasks.json file
+app.post('/', (req, res) => {
+    let task_form = req.body
+
+    if (validate.isEmpty(task_form.title) || validate.isEmpty(task_form.description)) {
+        res.redirect('/?received=no')
+    } else {
+        let rawdata = fs.readFileSync('tasks.json')
+        let data = JSON.parse(rawdata);
+
+        let task = {
+            title: task_form.title,
+            description: task_form.description,
+            priority: task_form.priority
+        }
+        data.push(task)
+        fs.writeFileSync('tasks.json', JSON.stringify(data))
+        res.redirect(`/?received=yes&priority=${task_form.priority}`)
+    }
+})
+// Loads the task page
 app.get('/tasks', (req, res) => {
     res.render('task')
 })
 
+// Creates a server and console logs the port number
 app.listen(port, () => { console.log(`Server is running on port ${port}`) })
 
